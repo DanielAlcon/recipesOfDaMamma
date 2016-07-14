d70f02f9fb40df4e3423da6f32d1e0cff531ac2b
DanielAlcon <daniel.alcon.diaz@gmail.com>
2016-07-14 17:10:04 +0200

    login correct. lack saving in localstorage and/or cookies

--- js/app.js
+++ js/app.js
@@ -3,26 +3,39 @@ var app = angular.module('recipesApp', ['ngRoute']);
 app.config(['$routeProvider',function($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl: 'views/expenses.html',
+    templateUrl: 'views/login.html',
     controller: 'HomeViewController'
-  })
-  .when('/expenses', {
-    templateUrl: 'views/expenses.html',
-    controller: 'ExpensesViewController'
-  })
-  .when('/expenses/new' , {
-    templateUrl: 'views/expensesForm.html',
-    controller: 'NewEditExpenseViewController'
-  })
+  })/*
   .when('/expenses/edit/:id' , {
     templateUrl: 'views/expensesForm.html',
     controller: 'NewEditExpenseViewController'
   })
   .otherwise({
     redirectTo: '/'
-  });
+  });*/
 }]);
 
 app.controller('HomeViewController', ['$scope', function($scope){
-  $scope.appTitle = 'Simple Expenses Tracker. OU Yeah!!!';
 }]);
+
+app.directive('googleLogin', function(){
+	var injectScript = function(element) {
+        var scriptTag = angular.element(document.createElement('script'));
+        scriptTag.attr('charset', 'utf-8');
+        scriptTag.attr('src', 'https://apis.google.com/js/platform.js', 'async', 'defer');
+        element.append(scriptTag);
+    };
+
+    // var injectScript2 = function(element) {
+    //     var scriptTag = angular.element(document.createElement('script'));
+    //     scriptTag.attr('charset', 'utf-8');
+    //     scriptTag.attr('src', 'https://apis.google.com/js/platform.js', 'async', 'defer');
+    //     element.append(scriptTag);
+    // };
+
+    return {
+        link: function(scope, element) {
+            injectScript(element);
+        }
+    };
+});
